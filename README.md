d1648edcbaecdafb82abaea0c17a9d36fdb3e101
rherd <rob.a.herd@gmail.com>
2018-06-05 19:07:17 -0400

    final

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Psychic-Game
 
 
-This is a game. In this game, the computer will choose a random letter. The player has 9 guesses to pick the right letter, which you can do by pressing any letter on the keyboard. The game will keep track of your guesses so far. After you run out of guesses, you will have a loss in the "loss" column; similarly, if you win the game, the game will update your "wins" score. 
+In this game, the computer will choose a random letter. The player has 9 guesses to pick the right letter, which you can do by pressing any letter on the keyboard. The game will keep track of your guesses so far. After you run out of guesses, you will have a loss in the "loss" column; similarly, if you win the game, the game will update your "wins" score. 
 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
-//Array of possible computer choices
+//the alphabet
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
-//Variables for tracking players wins, losses, & guesses left
+// instantiate variables
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
@@ -10,21 +10,22 @@ var myGuesses = [];
 var computerGuesses = [];
 
 
-//Initial computer letter
-// window.onload = function() {
+//initialize guess
     var compGuess = letters[Math.floor(Math.random() * letters.length)];
-    //picks a random letter, pushes it to the array
+    //picks a random letter, pushes it to the array at the end so that the most recent computer guess is the one we're comparing against
     computerGuesses.push(compGuess);
     console.log(computerGuesses);
-// }
+
 
 
 //Game
+//when the user presses a key, add it to the array of guesses at the end using push
 document.onkeyup = function(event) {
 	var playerGuess = event.key;
 	myGuesses.push(playerGuess);
     console.log(myGuesses);
 
+// if the player guess equals the current computer guess and you have any guesses left, add one to the wins, reset myGuesses, reset the counter of guesses left to 9, come up with a new guess
 if ((playerGuess === computerGuesses[0]) && (guessesLeft > 0)) {
 	wins++;
 	guessesLeft = 9;
@@ -35,10 +36,13 @@ if ((playerGuess === computerGuesses[0]) && (guessesLeft > 0)) {
     console.log(myGuesses);
 }
 
+//if you guess wrong and you still have guesses remaining, lose a guess
 else if ((playerGuess !== computerGuesses[0]) && (guessesLeft > 0)) {
 	guessesLeft = guessesLeft-1;
 }
 
+//the remaining scenario is if you guess wrong and have no guesses remaining
+//add one to the loss column, restart the game, reset guesses remaining
 else {
 	losses++;
 	guessesLeft = 9;
@@ -49,12 +53,14 @@ else {
     console.log(computerGuesses)
 }
 
-var html = "<p>Guess what letter I'm thinking of!</p>" +
+
+//update html with new wins, losses, guessesLeft, myGuesses
+var updatedHtml = "<p>Guess what letter I'm thinking of!</p>" +
           "<p>Wins: " + wins + "</p>" +
           "<p>Losses: " + losses + "</p>" +
           "<p>Guesses left: " + guessesLeft + "</p>" +
           "<p>Your guesses so far: " + myGuesses + "</p>";
  
-document.querySelector("#game").innerHTML = html;
+document.querySelector("#game").innerHTML = updatedHtml;
 
 }
